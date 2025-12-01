a7d535fa54c715ceffcea6349847ec4d1ea73a51
Eduardo Canales <131183212+educanales@users.noreply.github.com>
2025-12-01 12:50:49 -0300

    Test with empty array and concat

--- fibonacci.js
+++ fibonacci.js
@@ -3,7 +3,7 @@ function fibs(n) {
   let f1 = 0;
   let f2 = 1;
   myArray.push(f1, f2);
-  
+
   for (let i = 0; i < n - 2; i++) {
     let sum = f1 + f2;
     myArray.push(sum);
@@ -14,20 +14,28 @@ function fibs(n) {
   // return myArray;
 }
 
-// fibs(8);
+fibs(8);
 
 function fibsRec(n) {
-  // let array = [];
-  // console.log("This was printed recursively");
-  if (n < 2) {
+  console.log("This was printed recursively");
+  console.log(n);
+  let result = 0;
+  if (n === 1) {
     // console.log(n);
-    // return array.concat(n);
-    // return n;
+    result = 1;
   } else {
     // console.log(n);
     // return array.concat(fibsRec(n - 1) + fibsRec(n - 2));
-    // return fibsRec(n - 1) + fibsRec(n - 2);
+    const f1 = fibsRec(n - 1);
+    const f2 = fibsRec(n - 2);
+    result = f1 + f2;
+    // return [].push(result);
   }
+  console.log(result);
+  return [].concat(result);
 }
 
-console.log(fibsRec(8));
+// console.log(fibsRec(3));
+// (3 - 1 + 3 - 2) = 2 + 1
+// (2 - 1 + 2 - 2) = 1 + 0
+// (1 - 1 + 1 - 2) = 0 + -1
