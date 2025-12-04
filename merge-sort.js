affa68f57896dcc83af1e58f1469fcb3397e99fc
Eduardo Canales <131183212+educanales@users.noreply.github.com>
2025-12-04 12:20:45 -0300

    Add a second if condition

--- merge-sort.js
+++ merge-sort.js
@@ -1,13 +1,32 @@
 function mergeSort(array) {
   if (array.length <= 1) {
     return array;
+  } else if (array.length === 2) {
+    const firstHalf = array[0];
+    const secondHalf = array[1];
+    const newArray = [];
+    if (firstHalf < secondHalf) {
+      newArray.push(firstHalf, secondHalf);
+      return newArray;
+    } else {
+      newArray.push(secondHalf, firstHalf);
+      return newArray;
+    }  
   } else {
     // Dividir el array en 2
     const arrayHalf = array.length / 2;
     const firstHalf = array.slice(0, arrayHalf);
-    const secondHalf = array.slice(arrayHalf)
-    
+    const secondHalf = array.slice(arrayHalf);
+    const compFirst = mergeSort(firstHalf);
+    const compSecond = mergeSort(secondHalf);
+    return compSecond;
   }
 }
 
-console.log(mergeSort([1, 2, 4, 7]));
+
+const myArray = [1, 7, 5];
+
+console.log(mergeSort(myArray));
+
+// console.log(myArray.length)
+
