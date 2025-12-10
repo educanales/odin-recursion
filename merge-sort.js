56bf53b4358ae7e5e548605504d2027e20530aff
Eduardo Canales <131183212+educanales@users.noreply.github.com>
2025-12-10 12:33:33 -0300

    Resolve the exercise

--- merge-sort.js
+++ merge-sort.js
@@ -1,32 +1,49 @@
 function mergeSort(array) {
   if (array.length <= 1) {
+    // Base case
     return array;
-  } else if (array.length === 2) {
-    const firstHalf = array[0];
-    const secondHalf = array[1];
-    const newArray = [];
-    if (firstHalf < secondHalf) {
-      newArray.push(firstHalf, secondHalf);
-      return newArray;
-    } else {
-      newArray.push(secondHalf, firstHalf);
-      return newArray;
-    }  
-  } else {
-    // Dividir el array en 2
-    const arrayHalf = array.length / 2;
-    const firstHalf = array.slice(0, arrayHalf);
-    const secondHalf = array.slice(arrayHalf);
-    const compFirst = mergeSort(firstHalf);
-    const compSecond = mergeSort(secondHalf);
-    return compSecond;
   }
+  // Dividir
+  const half = array.length / 2;
+  const firstHalf = array.slice(0, half);
+  const secondHalf = array.slice(half);
+
+  const first = mergeSort(firstHalf);
+  const second = mergeSort(secondHalf);
+  // Merge
+  const newArray = [];
+  while (first.length > 0 && second.length > 0) {
+    if (first[0] < second[0]) {
+      newArray.push(first[0]);
+      first.shift();
+    } else {
+      newArray.push(second[0]);
+      second.shift();
+    }
+  }
+  if (first.length === 0) {
+    newArray.push(...second);
+  }
+  if (second.length === 0) {
+    newArray.push(...first);
+  }
+  return newArray;
 }
 
-
-const myArray = [1, 7, 5];
+const myArray = [105, 79, 100, 110];
 
 console.log(mergeSort(myArray));
 
-// console.log(myArray.length)
+// Pseudo Code
 
+// Mientras ambos arrays tengan elementos:
+//   si first[0] es menor que second[0]
+//     push first[0] a nuevoArray
+//     shift first
+//   si no 
+//     push second[0] a nuevoArray
+//     shift second
+// Si first esta vacio
+//   push second a nuevoArray
+// si second esta vacio
+//   push first a nuevo array
